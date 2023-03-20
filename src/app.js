6380eac2c1ca6f219574af69d8c5851f314c6d41
ahmadelsheikh2001 <ahmadelsheikhali@gmail.com>
2023-03-20 21:27:22 +0200

    'reweeeeeeee'

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+const path = require('path');
+const publicDirectory = path.join(__dirname, '../public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+
+const viewsDirectory = path.join(__dirname, '../temp1/views');
+app.set('views', viewsDirectory);
+
+//////////////////////////////////////////////////////////////////
+
+var hbs = require('hbs');
+
+const partialsPath = path.join(__dirname, '../temp1/partials');
+
+hbs.registerPartials(partialsPath);
+
+///////////////////////////////////////////////////////////////////
+
+app.get('/', (req, res) => {
+  res.render('index', { header: 'header', navbar: 'navbar', footer: 'footer' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { header: 'header', navbar: 'navbar', footer: 'footer' })
+})
+
+app.get('/amenities', (req, res) => {
+  res.render('amenities', { header: 'header', navbar: 'navbar', footer: 'footer' })
+})
+app.get('/booking', (req, res) => {
+  res.render('booking', { header: 'header', navbar: 'navbar', footer: 'footer' })
+})
+app.get('/contact', (req, res) => {
+  res.render('contact', { header: 'header', navbar: 'navbar', footer: 'footer' })
+})
+app.get('/login', (req, res) => {
+  res.render('login', { header: 'header', navbar: 'navbar', footer: 'footer' })
+})
+app.get('/room', (req, res) => {
+  res.render('room', { header: 'header', navbar: 'navbar', footer: 'footer' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
